e529ef2e5e11207c4efb144f3e8a1046ced34696
Khoa Bui <buihdk@gmail.com>
2018-09-16 20:27:27 +0700

    add static pages: about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/contact'
+
+  get 'pages/about'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'home#index'
