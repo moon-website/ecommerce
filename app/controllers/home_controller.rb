84dc1df2750d2bb78f0390fb817142675e713f1f
sunnyvo <votanphu.2101@gmail.com>
2018-09-25 08:27:14 +0700

    fix bug paginate and priority

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @courses          = Course.limit(3)
-    @service_eyebrown  = Service.where(order: "01").first
-    @service_eyelashes = Service.where(order: "02").first
-    @service_nails     = Service.where(order: "03").first
+    @service_eyebrown  = Service.where(priority: "01").first
+    @service_eyelashes = Service.where(priority: "02").first
+    @service_nails     = Service.where(priority: "03").first
     @categories        = Category.limit(3)
     # @catagories.includes(:products)
   end
---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,12 @@
+# Initialize if we use will_paginate
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+        alias_method :total_count, :count
+      end
+    end
+  end
+end
